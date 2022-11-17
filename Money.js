ec92d3b424c3a25304e4d9bb9dfbb0bb0642cd76
Winser Espinal <Wespinal@vivaresorts.com>
2022-11-17 15:32:23 -0400

    Added API

---
+++ Money.js
@@ -0,0 +1,10 @@
+var requestURL = 'https://api.exchangerate.host/2020-04-04';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var response = request.response;
+  console.log(response);
+}
