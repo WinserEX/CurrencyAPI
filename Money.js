a23d09718d9403b8134fea17c6858be39b481136
Winser Espinal <Wespinal@vivaresorts.com>
2022-11-17 18:06:01 -0400

    updated

--- Money.js
+++ Money.js
@@ -1,21 +1,41 @@
-var requestURL = 'https://api.exchangerate.host/2020-04-04';
+//Native Js API Call
+
+/* var requestURL = 'https://api.exchangerate.host/2020-04-04';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
-request.responseType = 'json';
+//request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-  var response = request.response;
-  console.log(response);
+  console.log("Success!!!");
+  const data = JSON.parse(this.responseText);
+  console.log(data.base)
+  console.log(data)
 }
 
+request.onerror = function() {
+  console.log("ERROR!!!");
+  console.log(this);
+} */
 
+//DOM test
 
-const p = document.createElement('P');
+/* const p = document.createElement('P');
 
 const container = document.body.querySelector("#target");
 
 container.append(p);
 
-p.innerHTML = "hello";
+p.innerHTML = "hello"; */
+
+//Fetch
+
+fetch("https://api.exchangerate.host/2020-04-04")
+.then(res => {
+    console.log("RESOLVED!", res);
+    res.json().then((data) => console.log("JSON DONE", data));
+})
+.catch((e) => {
+    console.log("ERROR!", e); 
+})
 
